9fd81dc3299468dd620f3a83cd7a6cba3f8c2519
Silviu Simen <ssimen@blackberry.com>
2022-07-01 20:08:27 -0400

    Convenience filter

--- m3u-filter2.php
+++ m3u-filter2.php
@@ -18,22 +18,27 @@
 
     $debug = @urldecode($_REQUEST['debug']);
     $m3u = urldecode($_REQUEST['m3u']);
-    $filter = urldecode($_REQUEST['filter']);
-    
-    $filter_cfg = load_json_from_file($filter);
+    //$filter = urldecode($_REQUEST['filter']);
+    $filters = explode(",",urldecode($_REQUEST['filters']));
+    //print_r($filters); die;
+
+
     $m3u_string = file_get_contents($m3u);
     $m3u_entries = m3u_to_json($m3u_string);
-    //$groups = get_list_of_groups($m3u_entries);
-    //print_r($groups);
-    //print_r($filter_cfg);
 
-    $filtered_entries = filter_entries($m3u_entries, $filter_cfg);
-    //print_r($filtered_entries);
-    
+    $filtered_entries = array();
+    foreach ($filters as $filter)
+    {
+        $filter_cfg = load_json_from_file($filter);
+        $new_entries = filter_entries($m3u_entries, $filter_cfg);
+        //print_r($new_entries); 
+        $filtered_entries = array_merge($filtered_entries, $new_entries);
+    }
+    //die;
 
     if ($debug == "true")
     {
-        out("Filter=".print_r($filter_cfg, true));
+        out("Filters=".print_r($filters, true));
         debug_list_m3u($filtered_entries);
     }
     else
---
+++ ss.php
@@ -0,0 +1,5 @@
+<?php
+
+header("Location: m3u-filter2.php?m3u=KY.m3u&filters=KY-discover.json,KY-sports.json,KY-reruns.json"); 
+
+?>
